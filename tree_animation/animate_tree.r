e01fa7eee7664d673048d94219cd7d69db9344bd
Dariya Sydykova <dariyasydykova@Dariyas-MacBook-Pro.local>
2018-11-19 19:07:42 -0600

    Updating code to animate sequences evolving along a tree

--- tree_animation/animate_tree.r
+++ tree_animation/animate_tree.r
@@ -4,7 +4,6 @@ library(dplyr)
 library(tidyr)
 library(cowplot)
 library(gganimate)
-library(magick)
 
 # make a data frame for a tree
 tree_df <- data.frame(time = c(0, 5, 0, 5, 3, 5, 3, 5),
@@ -19,6 +18,17 @@ tree_df <- data.frame(time = c(0, 5, 0, 5, 3, 5, 3, 5),
                                    "IPHGENRR…",
                                    "IPHGENRR…"))
 
+# seq_df <- data.frame(time = 5,
+#                      position = c(0, 4, 6, 10),
+#                      sequence = c("EPNGENRR…",
+#                                   "KPNGESDK…",
+#                                   "DPHGESFF…",
+#                                   "IPHGENRR…"))
+
+# subs_df <- data.frame(time = c(1, 2.5, 2, 2.5, 4, 4.5),
+#                       position = c(6, 7.5, 3, 2.5, 1, 9.5),
+#                       type = c(1:6))
+
 ancestor_seq <- "DPHGESFF…"
 
 ggplot(tree_df, aes(x = time, y = position, group = branch)) + 
@@ -27,15 +37,16 @@ ggplot(tree_df, aes(x = time, y = position, group = branch)) +
   scale_x_continuous(limits = c(0,10), breaks = c(1:10)) +
   background_grid(major = "xy") 
 
-ggplot(tree_df, aes(x = time, y = position, group = branch)) + 
-  geom_line(size = 1.5) + 
+ggplot(data = tree_df, mapping = aes(x = time, y = position)) + 
+  geom_line(aes(group = branch), size = 1.5) + 
   #geom_segment(aes(xend = time, yend = position), linetype = 2, colour = 'grey') + 
-  geom_text(aes(x = time, y = position, label = sequence), 
+  geom_point(size = 3) +
+  geom_text(aes(label = sequence), 
             hjust = 0, 
             nudge_x = 0.2,
             size = 6,
             family = "Courier") + 
-  geom_text(aes(x = 0, y = 5,label = ancestor_seq), 
+  geom_text(aes(x = 0, y = 5, label = ancestor_seq), 
             hjust = 1,
             nudge_x = -0.2,
             size = 6,
@@ -48,6 +59,7 @@ ggplot(tree_df, aes(x = time, y = position, group = branch)) +
         axis.ticks = element_blank(),
         axis.title.x = element_blank(),
         axis.title.y = element_blank(),
+        legend.position = "none",
         plot.background = element_blank(),
         plot.margin = margin(5.5, 100, 5.5, 100))
 
