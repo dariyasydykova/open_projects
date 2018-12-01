1fd2772b78bee3d21dddfd2645746ae0253fe003
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2018-12-01 17:41:39 -0600

    updating

--- tree_animation/animate_tree.r
+++ tree_animation/animate_tree.r
@@ -9,8 +9,8 @@ library(gganimate)
 tree_df <- data.frame(time = c(0, 1, 2, 5, 0, 5, 3, 5, 3, 5),
                       position = c(5, 6, 7, 10, 5, 0, 8, 6, 2, 4),
                       branch = c('b1', 'b1', 'b1', 'b1', 'b2', 'b2', 'b3', 'b3', 'b4', 'b4'),
-                      sequence = c("DPHGESFF…",
-                                   "EPNGESFF…",
+                      sequence = c("DP GESFF…",
+                                   "DP GESFF…",
                                    "DPNGESFF…",
                                    "EPNGESFF…",
                                    "KPNGESDK…",
@@ -18,7 +18,27 @@ tree_df <- data.frame(time = c(0, 1, 2, 5, 0, 5, 3, 5, 3, 5),
                                    "DPHGESFF…",
                                    "DPHGESFF…",
                                    "IPHGENRR…",
-                                   "IPHGENRR…"))
+                                   "IPHGENRR…"),
+                      subs = c("  H     ",
+                               "  N     ",
+                               "DPNGESFF…",
+                               "EPNGESFF…",
+                               "KPNGESDK…",
+                               "KPNGESDK…",
+                               "DPHGESFF…",
+                               "DPHGESFF…",
+                               "IPHGENRR…",
+                               "IPHGENRR…"),
+                      type = c("#5893d4",
+                               "#f7aa00", 
+                               "black",
+                               "black",
+                               "black",
+                               "black",
+                               "black",
+                               "black",
+                               "black",
+                               "black"))
 
 # seq_df <- data.frame(time = 5,
 #                      position = c(0, 4, 6, 10),
@@ -42,26 +62,24 @@ ggplot(tree_df, aes(x = time, y = position, group = branch)) +
 ggplot(data = tree_df, mapping = aes(x = time, y = position)) + 
   geom_line(aes(group = branch), size = 1.5) + 
   #geom_segment(aes(xend = time, yend = position), linetype = 2, colour = 'grey') + 
-  geom_point(aes(color = sequence), size = 3) +
-  geom_text(aes(label = sequence), 
-            hjust = 0, 
+  #geom_point(aes(color = sequence), size = 3) +
+  geom_text(aes(label = sequence),
+            hjust = 0,
             nudge_x = 0.2,
             size = 6,
-            family = "Courier") + 
-  geom_text(aes(x = 0, y = 5, label = ancestor_seq), 
+            family = "Courier",
+            color = "black") +
+  geom_text(aes(label = subs, color = type),
+            hjust = 0,
+            nudge_x = 0.2,
+            size = 6,
+            family = "Courier") +
+  geom_text(aes(x = 0, y = 5, label = ancestor_seq),
             hjust = 1,
             nudge_x = -0.2,
             size = 6,
-            family = "Courier") + 
+            family = "Courier") +
   transition_reveal(branch, time) + 
-  coord_cartesian(clip = 'off') + 
-  theme(axis.line = element_blank(), # remove axes, axes labels, and background from the plot
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        legend.position = "none",
-        plot.background = element_blank(),
-        plot.margin = margin(5.5, 100, 5.5, 100))
+  coord_cartesian(clip = 'off', xlim = c(-2, 7)) +
+  theme_void()
 
