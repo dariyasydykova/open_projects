f859474d94667b4ee86d4a7be5cdcb338e7bcaa6
Dariya Sydykova <dariya.k.sydykova@gmail.com>
2018-12-12 16:41:45 -0600

    Updating a script

--- ROC_animation/animate_SD.r
+++ ROC_animation/animate_SD.r
@@ -15,15 +15,34 @@ calc_ROC <- function(probabilities, known_truth, model.name = NULL)
   pos_probs <- outcome*probabilities # probabilities for known positives
   neg_probs <- (1-outcome)*probabilities # probabilities for known negatives
   true_pos <- sapply(probabilities,
-                     function(x) sum(pos_probs>= x)/pos) # true pos. rate
+                     function(x) sum(pos_probs >= x)/pos) # true pos. rate
   false_pos <- sapply(probabilities,
-                      function(x) sum(neg_probs>= x)/neg)
+                      function(x) sum(neg_probs >= x)/neg)
   if (is.null(model.name))
     result <- data.frame(true_pos, false_pos)
   else
     result <- data.frame(true_pos, false_pos, model.name)
   
   result %>% add_row(true_pos = 0, false_pos = 0) %>% arrange(false_pos, true_pos)
+}
+
+# this function calculates precision and recall
+calc_PR <- function(probabilities, known_truth, model.name = NULL)
+{
+  outcome <- as.numeric(factor(known_truth))-1
+  pos <- sum(outcome) # total known positives
+  pos_probs <- outcome*probabilities # probabilities for known positives
+  neg_probs <- (1-outcome)*probabilities # probabilities for known negatives
+  recall <- sapply(probabilities,
+                     function(x) sum(pos_probs >= x)/pos) 
+  precision <- sapply(probabilities,
+                      function(x) sum(pos_probs >= x)/(sum(pos_probs >= x)+sum(neg_probs >= x)))
+  if (is.null(model.name))
+    result <- data.frame(precision, recall)
+  else
+    result <- data.frame(precision, recall, model.name)
+  
+  result %>% arrange(recall, desc(precision))
 }
 
 # make a reduced iris data set that only contains virginica and versicolor species
@@ -47,6 +66,7 @@ vers_t1 <- lr_data %>% filter(Species == "versicolor") %>% mutate(predictor = pr
 virg_t1 %>% mutate(predictor = mean(predictor) + 0.4*(predictor - mean(predictor)), time = 2) -> virg_t2
 vers_t1 %>% mutate(time = 2) -> vers_t2
 
+# combine all data sets together
 rbind(virg_t1, virg_t2, vers_t1, vers_t2) -> anim_data
 
 # make an animation with distributions of linear predictors
@@ -60,7 +80,7 @@ p_dist <- ggplot(anim_data, mapping = aes(predictor, fill = Species)) +
   theme_cowplot() +
   theme(legend.position = "none")
 
-# combine all data frames and calculate ROC curves and AUC values
+# calculate ROC curves and AUC values
 anim_data %>%
   mutate(probabilities = exp(predictor)/(1+exp(predictor))) %>% # calculate probabilities for linear predictors
   group_by(time) %>% 
@@ -71,6 +91,15 @@ anim_data %>%
   mutate(delta = false_pos - lag(false_pos)) %>%# calculate AUC values
   mutate(AUC = sprintf("%.3f", sum(delta*true_pos, na.rm = T))) -> ROC
 
+# calculate precision-recall curves
+anim_data %>%
+  mutate(probabilities = exp(predictor)/(1+exp(predictor))) %>% # calculate probabilities for linear predictors
+  group_by(time) %>% 
+  do(results = calc_PR(probabilities = .$probabilities, # calculate precision and recall
+                        known_truth = .$Species)) %>%
+  group_by(time) %>%
+  do(as.data.frame(.$results)) -> PR
+
 # make an animation with ROC curves
 p_ROC <- ggplot(data = ROC, aes(x = false_pos, y = true_pos)) +
   geom_line(size = 1) +
@@ -79,6 +108,13 @@ p_ROC <- ggplot(data = ROC, aes(x = false_pos, y = true_pos)) +
   ggtitle("AUC = {closest_state}") +
   scale_x_continuous(name = "false positive rate") +
   scale_y_continuous(name = "true positive rate") +
+  theme_cowplot()
+
+# make an animation with precision-recall curves
+p_PR <- ggplot(data = PR, aes(x = recall, y = precision)) +
+  geom_line(size = 1) +
+  transition_states(time, transition_length = 1, state_length = 1) +
+  ggtitle(" ") + # adding an emtpy title to align y axis with ROC plot
   theme_cowplot()
 
 # save each animation as individual frames
@@ -93,18 +129,25 @@ p_ROC_gif <- animate(p_ROC,
                      width = 400, 
                      height = 400,
                      renderer = file_renderer("./gganim_SD", prefix = "p_ROC", overwrite = TRUE))
+p_PR_gif <- animate(p_PR, 
+                     device = "png",
+                     width = 400, 
+                     height = 400,
+                     renderer = file_renderer("./gganim_SD", prefix = "p_PR", overwrite = TRUE))
 
-# stitch two animations together
+# stitch animations together
 # read the first image (frame) of each animation
 a <- image_read(p_dist_gif[[1]])
 b <- image_read(p_ROC_gif[[1]])
+c <- image_read(p_PR_gif[[1]])
 # combine the two images into a single image
-combined <- image_append(c(a, b))
+combined <- image_append(c(a, b, c))
 new_gif <- c(combined)
 for(i in 2:100){ # combine images frame by frame
   a <- image_read(p_dist_gif[[i]])
   b <- image_read(p_ROC_gif[[i]])
-  combined <- image_append(c(a, b))
+  c <- image_read(p_PR_gif[[i]])
+  combined <- image_append(c(a, b, c))
   new_gif <- c(new_gif, combined)
 }
 
